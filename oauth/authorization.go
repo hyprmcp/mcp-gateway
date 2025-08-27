cfb08378351b05ac3e78f004439d261182da619f
Jakob Steiner <jakob.steiner@glasskube.eu>
2025-08-27 16:42:15 +0200

    chore: also inject profile, email scopes

--- oauth/authorization.go
+++ oauth/authorization.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"slices"
 	"strings"
 
 	"github.com/hyprmcp/mcp-gateway/config"
@@ -13,6 +14,12 @@ import (
 const AuthorizationPath = "/oauth/authorize"
 
 func NewAuthorizationHandler(config *config.Config, meta map[string]any) (http.Handler, error) {
+	supportedScopes := getSupportedScopes(meta)
+	var requiredScopes = slices.DeleteFunc(
+		[]string{"openid", "profile", "email"},
+		func(s string) bool { return !slices.Contains(supportedScopes, s) },
+	)
+
 	if authorizationEndpointStr, ok := meta["authorization_endpoint"].(string); !ok {
 		return nil, errors.New("authorization metadata is missing authorization_endpoint field")
 	} else if _, err := url.Parse(authorizationEndpointStr); err != nil {
@@ -21,9 +28,13 @@ func NewAuthorizationHandler(config *config.Config, meta map[string]any) (http.H
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			redirectURI, _ := url.Parse(authorizationEndpointStr)
 			q := r.URL.Query()
-			if scope := q.Get("scope"); !strings.Contains(scope, "openid") {
-				q.Set("scope", strings.TrimSpace("openid "+scope))
+			scopes := q.Get("scope")
+			for _, scope := range requiredScopes {
+				if !strings.Contains(scopes, scope) {
+					scopes = strings.TrimSpace(scopes + " " + scope)
+				}
 			}
+			q.Set("scope", scopes)
 			redirectURI.RawQuery = q.Encode()
 			http.Redirect(w, r, redirectURI.String(), http.StatusFound)
 		}), nil
